25d96fd9be127a3d7d623ddf97be8605366a3182
LAPTOP-NLCHTG5Q\user <rogeroasis0709@gmail.com>
2025-05-20 21:37:26 +0800

    theme settin

--- src/Main.js
+++ src/Main.js
@@ -2,7 +2,8 @@
 import './Main.css'
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { ChakraProvider, Button } from '@chakra-ui/react';
+import { ChakraProvider, Button, theme, Box } from '@chakra-ui/react';
+import customTheme from './theme';
 
 function Main() {
   const navigate = useNavigate();
@@ -12,10 +13,14 @@ function Main() {
   };
 
   return (
-    <div className='MainTopic'>
-      <h1>首頁</h1>
-      <button onClick={handleClick}>跳轉到 Service 頁面</button>
-    </div>
+    <ChakraProvider theme={customTheme}>
+        <div className='MainTopic'>
+          <h1>高雄市榮服處管理系統</h1>
+        </div>
+        <div className='StartButton'>
+          <Button onClick={handleClick}>Start Service</Button>
+        </div>
+    </ChakraProvider>
   );
 }
 
--- src/theme.js
+++ src/theme.js
@@ -1,25 +1,42 @@
 // theme.js
-import { extendTheme } from "@chakra-ui/react";
+import { extendTheme, withDefaultColorScheme } from "@chakra-ui/react";
 
-const customTheme = extendTheme({
-  colors: {
-    brand: {
-      100: "#f7fafc",
-      500: "#2b6cb0", // 主色
-      900: "#1a202c",
+// 色彩盤
+const colors = {
+  gray: {
+    900: '#262626',
+    800: '#323232',
+    700: '#434343',
+    600: '#595959',
+    500: '#8c8c8c',
+    400: '#BFBFBF',
+    300: '#d9d9d9',
+    200: '#f0f0f0',
+    100: '#f5f5f5',
+    50: '#fafafa'
+  }
+};
+
+// 主題顏色
+const styles = {
+  global: {
+    body: {
+      bg: 'gray.900',     // 設定背景為深灰
+      color: 'gray.50',   // 設定文字顏色為淺灰/白
     },
   },
-  fonts: {
-    heading: "Poppins, sans-serif",
-    body: "Roboto, sans-serif",
+};
+
+
+const customTheme = extendTheme(
+  {
+    colors,
+    styles
   },
-  components: {
-    Button: {
-      baseStyle: {
-        borderRadius: "xl",
-      },
-    },
-  },
-});
+  withDefaultColorScheme({
+    colorScheme: 'blue',
+    components: ['Button'], // 指定哪些元件使用預設色系
+  })
+);
 
 export default customTheme;
